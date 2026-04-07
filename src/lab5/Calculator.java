dbf8f6d944aeacfbb7ec51a7c27ca651a076051c
Patrick <patrickgeorge.rucarean@ulbsibiu,ro>
2026-04-08 01:39:45 +0300

    a)

---
+++ src/lab5/Calculator.java
@@ -0,0 +1,33 @@
+package lab5;
+
+public class Calculator {
+    protected int state;
+
+    public Calculator(int state) {
+        this.state = state;
+    }
+
+    public Calculator add(int value) {
+        this.state += value;
+        return this;
+    }
+
+    public Calculator subtract(int value) {
+        this.state -= value;
+        return this;
+    }
+
+    public Calculator multiply(int value) {
+        this.state *= value;
+        return this;
+    }
+
+    public int result() {
+        return this.state;
+    }
+
+    public Calculator clear() {
+        this.state = 0;
+        return this;
+    }
+}
